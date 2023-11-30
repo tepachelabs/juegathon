a9888dd29a19e66e2b4b2b7516df3269dc27b2df
tonymtz <atony.mtz@gmail.com>
2023-11-29 20:17:57 -0700

    theme is taken from posthog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { ThemeProvider } from "@emotion/react";
-import { useFeatureFlagEnabled } from "posthog-js/react";
+import { useFeatureFlagEnabled, useFeatureFlagPayload } from "posthog-js/react";
 
 import { Faq, Hero, Page, PageMeta, Press, Schedule } from "../src/components";
 import { LiveTemplate } from "../src/templates/live.template";
 import { theme2023 } from "../src/themes/theme-2023";
 import data from "../src/data/schedule-2023.json";
+import { theme2022 } from "../src/themes/theme-2022";
 
 const generateMeta = (isLive: boolean) => {
   const meta: PageMeta = {
@@ -18,16 +19,22 @@ const generateMeta = (isLive: boolean) => {
   return meta;
 };
 
+const getYearTheme = (year?: number) => {
+  if (year === 2023) return theme2023;
+  return theme2022;
+}
+
 export default function Home() {
   const router = useRouter();
   const { live } = router.query;
-  const showTwitchIframe = useFeatureFlagEnabled('show-twitch-iframe')
+  const showTwitchIframe = useFeatureFlagEnabled('show-twitch-iframe');
+  const websiteTheme = useFeatureFlagPayload('website-theme') as number;
   const isLive = Boolean(live) || showTwitchIframe || false;
   const meta = generateMeta(isLive);
   const { saturday, sunday } = data.schedules;
 
   return (
-    <ThemeProvider theme={ theme2023 }>
+    <ThemeProvider theme={ getYearTheme(websiteTheme) }>
       <Page meta={ meta }>
         { isLive ? <LiveTemplate/> :
           <Hero className="pt-4 mb-4" year={ 2023 } dateLabel="25 y 26 de Noviembre"/> }
