3d98d3937aee28ec882e5d6ba4f34875b7d381c0
tonymtz <atony.mtz@gmail.com>
2023-11-29 19:55:41 -0700

    removes feature-flag from notion, consumes posthog now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { ThemeProvider } from "@emotion/react";
+import { useFeatureFlagEnabled } from "posthog-js/react";
 
 import { Faq, Hero, Page, PageMeta, Press, Schedule } from "../src/components";
 import { LiveTemplate } from "../src/templates/live.template";
-import { fetchConfig } from "../src/remote-config";
 import { theme2023 } from "../src/themes/theme-2023";
 import data from "../src/data/schedule-2023.json";
 
@@ -18,26 +18,18 @@ const generateMeta = (isLive: boolean) => {
   return meta;
 };
 
-export async function getServerSideProps() {
-  const config = await fetchConfig();
-
-  return {
-    props: { config },
-  };
-}
-
-export default function Home(props: any) {
-  const { config } = props;
+export default function Home() {
   const router = useRouter();
   const { live } = router.query;
-  const meta = generateMeta(config.live);
-
+  const showTwitchIframe = useFeatureFlagEnabled('show-twitch-iframe')
+  const isLive = Boolean(live) || showTwitchIframe || false;
+  const meta = generateMeta(isLive);
   const { saturday, sunday } = data.schedules;
 
   return (
     <ThemeProvider theme={ theme2023 }>
       <Page meta={ meta }>
-        { config.live || live ? <LiveTemplate/> :
+        { isLive ? <LiveTemplate/> :
           <Hero className="pt-4 mb-4" year={ 2023 } dateLabel="25 y 26 de Noviembre"/> }
         <Schedule className="my-5" saturday={ saturday } sunday={ sunday }/>
         <Faq className="mt-5 pb-5"/>
